299968786589aba2a022aa8a3ed3db96d11dc0e0
ji-mmyliu <jimmyjcl753@gmail.com>
2023-10-07 16:40:52 -0400

    Initial commit

---
+++ client/components/home/index.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { default as axios } from "axios";
+
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Typography } from "@mui/material";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+export const Home = (): JSX.Element => {
+    return (
+        <>
+            {/* <Box sx={{ flexGrow: 1, padding: "1em" }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={8}>
+                        <Item>
+                            <Typography variant="h2">
+                                Wanna learn something?
+                            </Typography>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Item>
+                            <img src="/static/img/learning-1.jpg" width="20%"></img>
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box> */}
+            {/* <Box sx={{backgroundColor: "lightpurple"}}> */}
+            <div className="jumbotron" style={{padding: "1em"}}>
+                <h2 className="display-4">Wanna learn something?</h2>
+                <h1 className="display-1">Meet <strong>QED</strong></h1>
+                <h2 className="display-3">It's already been proven.</h2>
+
+                {/* <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-4" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">
+                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                </p> */}
+
+                <img src="/static/img/learning-1.jpg" width="100%"></img>
+            </div>
+            {/* </Box> */}
+        </>
+    );
+}
---
+++ client/util/routes/routes.ts
@@ -0,0 +1,9 @@
+let BASEURL = "http://localhost:8000"
+let Routes = {
+    GRADES: {
+        ALL: `${BASEURL}/grades/all`,
+        NEW: `${BASEURL}/grades/new`
+    }
+};
+
+export default Routes;
