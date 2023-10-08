95f9888173175d3e46a686b376f96f634ea2332c
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2023-10-07 20:05:26 -0400

    add token auth function

---
+++ server/lib/auth.js
@@ -0,0 +1,21 @@
+const config = require('../config');
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res) {
+    return new Promise((resolve, reject) => {
+        if (req.headers.authorization && req.headers.authorization.split(' ').length == 2) {
+            let token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, config.jwt, (err, user) => {
+                if (err) {
+                    reject('Invalid token');
+                }
+                else {
+                    resolve(user);
+                }
+            })
+        }
+        else {
+            reject('Missing or invalid authorization header');
+        }
+    })
+}
